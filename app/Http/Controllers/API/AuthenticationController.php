54052560182f839f797ec9afdac589eb8a806401
junaidsa <94611843+junaidsa@users.noreply.github.com>
2025-03-02 00:03:09 +0500

    done

--- app/Http/Controllers/API/AuthenticationController.php
+++ app/Http/Controllers/API/AuthenticationController.php
@@ -1,6 +1,7 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
+
+use App\Http\Controllers\API\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthenticationController;
-
 Route::post('/authenicate', [AuthenticationController::class, 'sendOtp'])->name('authenicate');
 Route::get('/test', [AuthenticationController::class, 'test'])->name('test');
 Route::group(['middleware' => 'auth:sanctum'], function () {
