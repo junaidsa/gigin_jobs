39ad11f6571aaa0c73f33cf42ff113060fbaca72
junaidsa <94611843+junaidsa@users.noreply.github.com>
2025-03-02 11:14:02 +0500

    login fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,78 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>CMS || Login</title>
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!-- loader-->
+    <link href="{{asset('assets/css/pace.min.css')}}" rel="stylesheet" />
+    <script src="{{asset('assets/js/pace.min.js')}}"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}" />
+    <!-- Icons CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/icons.css')}}" />
+    <!-- App CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}" />
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+
+<body class="bg-login">
+    <!-- wrapper -->
+    <div class="wrapper">
+        <div class="section-authentication-login d-flex align-items-center justify-content-center">
+            <div class="row">
+                <div class="col-12 col-lg-10 mx-auto">
+                    <div class="card radius-15">
+                        <div class="row no-gutters">
+                            <div class="col-lg-6">
+                                <div class="card-body p-md-5">
+                                    <div class="text-center">
+                                        <img src="assets/images/logo-icon.png" width="80" alt="">
+                                        <h3 class="mt-4 font-weight-bold">Welcome Back</h3>
+                                    </div>
+                                    <div class="login-separater text-center"> <span>LOGIN WITH EMAIL</span>
+                                        <hr />
+                                    </div>
+                                    <form form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group mt-4">
+                                            <x-input-label for="email" :value="__('Email')" />
+                                            <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                        <!-- Password -->
+                                        <div class="form-group mt-4">
+                                            <x-input-label for="password" :value="__('Password')" />
+                                            <x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" />
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div>
+                                        <div class="btn-group mt-3 w-100">
+                                            <x-primary-button class="btn btn-primary btn-block">
+                                                {{ __('Log in') }}
+                                            </x-primary-button>
+                                            <button type="button" class="btn btn-primary"><i class="lni lni-arrow-right"></i>
+                                            </button>
+                                        </div>
+                                    </form>
+                                    <hr>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="assets/images/login-images/login-frent-img.jpg" class="card-img login-img h-100" alt="...">
+                            </div>
+                        </div>
+                        <!--end row-->
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+    <!-- end wrapper -->
+</body>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
