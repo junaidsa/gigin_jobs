8efd55071ee032d7e09eb34c60c4d67c4d513e7f
junaidsa <94611843+junaidsa@users.noreply.github.com>
2025-03-02 19:02:13 +0500

    fix

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,10 +20,6 @@ class ProfileController extends Controller
             'user' => $request->user(),
         ]);
     }
-
-    /**
-     * Update the user's profile information.
-     */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
